660a57128953b8565900e6e27db8339fd2001a08
Your Name <89849826+vazs12@users.noreply.github.com>
2023-05-19 10:55:16 -0300

    index com modelo

--- itapira/enquete/urls.py
+++ itapira/enquete/urls.py
@@ -7,4 +7,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('tik', views.tik, name="tik"),
+    path("<int:questao_id>/", views.detalhe, name="detalhe"),
+     #ex: /enquete/5/
+    path("<int:questao_id>/resultados/", views.resultados, name="resultados"),
+    #ex: /enquete/5/resultados/     
+    path("<int:questao_id>/voto/", views.voto, name="voto"),
+    #ex: /enquete/5/voto/
 ]
+
--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Questao
 
 # Create your views here.
 def index (request):
-    return HttpResponse("I WAS ENCHANTED TO MEET YOU")
+    ultimas_questoes = Questao.objects.order_by("-data")[:5]
+    saida =", ".join([q.pergunta for q in ultimas_questoes])
+    return HttpResponse(saida)
 
 def tik (request):
     return HttpResponse("É REAL O QUE TU SENTE?")
+
+def detalhe (request,questao_id):
+    return HttpResponse("Você está olhando a questão %s" % questao_id)
+
+def resultados (request,questao_id):
+    response = "Você está olhando o resultado da questão %s"
+    return HttpResponse(response % questao_id)
+
+def voto (request,questao_id):
+    return HttpResponse("Você está votando na questão %s" % questao_id)
