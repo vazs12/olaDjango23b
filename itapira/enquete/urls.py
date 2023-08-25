a926bab7b7ff2d63448dc6f7719abaeb536eb0d3
Your Name <89849826+vazs12@users.noreply.github.com>
2023-08-25 09:39:15 -0300

    arquivos estaticos

--- itapira/enquete/urls.py
+++ itapira/enquete/urls.py
@@ -5,8 +5,7 @@ from . import views
 #ele é o controller que pega um padrão de url 
 #e envia para o usuario a view especifica
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('tik', views.tik, name="tik"),
+    path('', views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetalheView.as_view(), name="detalhe"),
      #ex: /enquete/5/
     path("<int:pk>/resultados/", views.ResultadoView.as_view(), name="resultados"),
